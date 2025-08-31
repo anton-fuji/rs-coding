20f53c1c009d5cb8d48ff3f123f5eef681f3e0bd
Fuji <fujiji.entire1414@gmail.com>
2025-08-31 18:19:37 +0900

    Some Sums

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,11 +1,30 @@
 use proconio::input;
 
+fn sum_digits(n: usize) -> usize {
+    let mut num = n;
+    let mut ans = 0;
+    while num >= 1 {
+        ans += num % 10;
+        num /= 10;
+    }
+    ans
+}
+
+fn solve(n: usize, a: usize, b: usize) -> usize {
+    (1..=n)
+        .into_iter()
+        .map(|n| (n, sum_digits(n)))
+        .filter(|t| t.1 >= a && t.1 <= b)
+        .map(|t| t.0)
+        .sum()
+}
+
 fn main() {
     input! {
-      s: String,
+      n: usize,
+      a: usize,
+      b: usize,
     }
 
-    let count = s.chars().filter(|&c| c == '1').count();
-
-    println!("{count}");
+    println!("{}", solve(n, a, b));
 }
