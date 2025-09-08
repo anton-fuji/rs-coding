ee3909104db5946f886e92bb7b44c54ccc76daa3
Fuji <fujiji.entire1414@gmail.com>
2025-09-08 12:57:03 +0900

    fix:Cargo.toml

--- LeetCode/easy/src/main.rs
+++ LeetCode/easy/src/main.rs
@@ -1,20 +1,17 @@
 struct Solution;
 
 impl Solution {
-    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        let mut k = 0;
-
-        for i in 0..nums.len() {
-            if nums[i] != val {
-                nums[k] = nums[i];
-                k += 1
-            }
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        match haystack.find(&needle) {
+            Some(idx) => idx as i32,
+            None => -1,
         }
-        k as i32
     }
 }
 
 fn main() {
-    let mut nums = vec![3, 2, 2, 3];
-    println!("{}", Solution::remove_element(&mut nums, 3));
+    let haystack = "leetcode".to_string();
+    let needle = "leeto".to_string();
+
+    println!("ans:{}", Solution::str_str(haystack, needle));
 }
