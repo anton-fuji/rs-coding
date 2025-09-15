a1db6dac77283239ed6706680eb81f5ffd3c72ca
Fuji <fujiji.entire1414@gmail.com>
2025-09-15 11:39:40 +0900

    abc423_A, B

---
+++ abc423/src/b.rs
@@ -0,0 +1,30 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+      n: usize,
+      l: [usize; n],
+    }
+
+    println!("{}", solve(n, &l));
+}
+
+fn solve(n: usize, l: &[usize]) -> usize {
+    let mut first = 1 << 60;
+    let mut last = 0;
+    for i in 0..n {
+        if l[i] == 0 {
+            continue;
+        }
+
+        first = first.min(i + 1);
+        last = last.max(i + 1);
+    }
+
+    if last == 0 {
+        return 0;
+    }
+
+    last - first
+}
