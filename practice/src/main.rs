46f2933182fcc73a58447c6e654d00e8ce29a573
Fuji <fujiji.entire1414@gmail.com>
2025-11-16 20:54:34 +0900

    fix

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -4,22 +4,16 @@ use proconio::{fastout, input, marker::Chars};
 #[fastout]
 fn main() {
     input! {
-      s :Chars,
+      v: Chars,
     }
 
-    let mut x = s
-        .iter()
-        .map(|&c| c.to_digit(10).unwrap() as i32)
-        .collect_vec();
-    x.push(0);
-    let y = (0..s.len())
-        .map(|i| ((x[i] - x[i + 1]) % 10 + 10) % 10)
-        .collect_vec();
-    println!("{}", sum(y));
-}
-
-fn sum(v: Vec<i32>) -> i32 {
-    let mut ret = v.iter().sum();
-    ret += v.len() as i32;
-    ret
+    let res = v
+        .clone()
+        .into_iter()
+        .permutations(v.len())
+        .filter(|f| f[0] != '0')
+        .map(|p| p.iter().collect::<String>())
+        .max()
+        .unwrap();
+    println!("{res}");
 }
