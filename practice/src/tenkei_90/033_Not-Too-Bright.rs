d0fa66172e1bdc6b386078b2265c4216d08d3dda
Fuji <fujiji.entire1414@gmail.com>
2025-12-08 20:51:17 +0900

    033_賢く解く系

---
+++ practice/src/tenkei_90/033_Not-Too-Bright.rs
@@ -0,0 +1,16 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+      h : usize,
+      w : usize,
+    }
+
+    if h == 1 || w == 1 {
+        println!("{}", h * w);
+    } else {
+        let res = ((h + 1) / 2) * ((w + 1) / 2);
+        println!("{res}");
+    }
+}
