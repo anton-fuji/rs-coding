f8272e2039e89193ea7110fbcce42d27f5ad3f6e
Fuji <fujiji.entire1414@gmail.com>
2025-12-30 13:54:05 +0900

    abc393_B

--- practice/src/abc393/b.rs
+++ practice/src/abc393/b.rs
@@ -1,30 +1,33 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
     input! {
-     s: String,
+      s:Chars
     }
 
-    let ss: Vec<char> = s.chars().collect();
-    let s_len = s.len();
-    let mut count = 0;
-
-    for j in 1..s_len - 1 {
-        if ss[j] == 'B' {
-            for d in 1..=s_len {
-                let i = j as isize - d as isize;
-                let k = j as isize + d as isize;
-
-                if i >= 0 && k < s_len as isize {
-                    if ss[i as usize] == 'A' && ss[k as usize] == 'C' {
-                        count += 1;
-                    }
-                } else {
-                    break;
+    let mut cnt = 0;
+    for i in 0..s.len() {
+        for j in 0..s.len() {
+            for k in 0..s.len() {
+                if i >= j || j >= k {
+                    continue;
                 }
+                if j - i != k - j {
+                    continue;
+                }
+                if s[i] != 'A' {
+                    continue;
+                }
+                if s[j] != 'B' {
+                    continue;
+                }
+                if s[k] != 'C' {
+                    continue;
+                }
+                cnt += 1;
             }
         }
     }
-    println!("{count}");
+    println!("{cnt}");
 }
