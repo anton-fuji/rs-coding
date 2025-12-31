9b12e3446a4f155593c592def5d590f09a3e05df
Fuji <fujiji.entire1414@gmail.com>
2025-12-31 12:11:06 +0900

    fix

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,38 +1,8 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
     input! {
-      n: usize,
-      l: usize,
-      k:usize,
-      mut a:[usize; n]
+      s:Chars
     }
-
-    a.push(l);
-    let mut left = 0;
-    let mut right = l;
-
-    while right - left > 1 {
-        let mid = (left + right) / 2;
-        if can(mid, k, &a) {
-            left = mid; // もっと大きくできる
-        } else {
-            right = mid; // 小さくする
-        }
-    }
-    println!("{left}");
-}
-
-fn can(x: usize, k: usize, a: &Vec<usize>) -> bool {
-    let mut cnt = 0;
-    let mut pre = 0;
-
-    for &pos in a.iter() {
-        if pos - pre >= x {
-            cnt += 1;
-            pre = pos;
-        }
-    }
-    cnt >= k + 1
 }
