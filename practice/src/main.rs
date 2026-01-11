c6d78533f9d429b3197634941023e6290cb75870
Fuji <fujiji.entire1414@gmail.com>
2026-01-11 18:06:00 +0900

    abc437_c

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,8 +1,33 @@
-use proconio::{fastout, input, marker::Chars};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-      s:Chars
+      t: usize,
+    }
+
+    for _ in 0..t {
+        input! {
+          n: usize,
+          wp: [(usize, usize); n]
+        }
+        let mut cost = Vec::with_capacity(n);
+        let mut cap = 0;
+        for (w, p) in wp {
+            cap += p;
+            cost.push(w + p);
+        }
+        cost.sort();
+        let mut used = 0;
+        let mut res = 0;
+        for c in cost {
+            if used + c > cap {
+                break;
+            } else {
+                used += c;
+                res += 1;
+            }
+        }
+        println!("{res}");
     }
 }
