03e0610e3c88ce0d9b5172ce2862e630886c9888
Fuji <fujiji.entire1414@gmail.com>
2026-03-03 21:57:36 +0900

    abc447

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,23 +1,22 @@
-use std::iter::repeat;
-
 use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 #[allow(unused_imports)]
 fn main() {
     input! {
-        n: usize,
-        s: [String; n],
+        s: String,
     }
 
-    let max_len = s.iter().map(|w| w.len()).max().unwrap();
-    for c in s {
-        let sub = max_len - c.len();
-        let d = sub / 2;
-        let l_d: String = repeat('.').take(d).collect();
-        let r_d: String = repeat('.').take(d).collect();
-
-        let res = format!("{}{}{}", l_d, c, r_d);
-        println!("{}", res);
+    let mut freq = [0; 26];
+    for c in s.chars() {
+        freq[(c as u8 - b'a') as usize] += 1;
     }
+
+    let max = *freq.iter().max().unwrap();
+
+    let res: String = s
+        .chars()
+        .filter(|&c| freq[(c as u8 - b'a') as usize] != max)
+        .collect();
+    println!("{}", res)
 }
