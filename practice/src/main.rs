aecf34378c988a68852d7eb69d7a1fca7ced0709
Fuji <fujiji.entire1414@gmail.com>
2026-03-16 21:26:49 +0900

    fix

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -6,14 +6,15 @@ fn main() {
     input! {
         n: usize,
         m: usize,
-        c: [usize; m]
+        c: [usize; m],
     }
 
     let mut p = vec![0; m + 1];
     for _ in 0..n {
-        input! {mut a: usize, b: usize}
+        input! {a: usize, b: usize}
         p[a - 1] += b;
     }
+
     let mut res = 0;
     for j in 0..m {
         res += p[j].min(c[j]);
