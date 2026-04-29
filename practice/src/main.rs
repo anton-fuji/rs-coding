3fb4011a97b8ea8e9cc9517f2c136d5baefa7c18
Fuji <fujiji.entire1414@gmail.com>
2026-04-29 09:36:42 +0900

    abc418_A

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,20 +1,15 @@
 use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
-#[allow(unused_imports)]
+#[allow(unused_imports, non_snake_case)]
 fn main() {
     input! {
-        n: usize,
-        m :usize
+        s: String,
     }
 
-    let mut diff = vec![0i64; m + 1];
-    for _ in 0..n {
-        input! {a:usize, b:usize}
-        diff[a] -= 1;
-        diff[b] += 1;
-    }
-    for i in 1..=m {
-        println!("{}", diff[i]);
+    if s.ends_with("tea") {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
