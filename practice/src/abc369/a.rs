7f83f767644305987cc58d3b587637e98345b06b
Fuji <fujiji.entire1414@gmail.com>
2026-05-28 19:17:31 +0900

    abc369_A

---
+++ practice/src/abc369/a.rs
@@ -0,0 +1,20 @@
+use proconio::{fastout, input, marker::Chars};
+
+#[fastout]
+#[allow(unused_imports, non_snake_case)]
+fn main() {
+    input! {
+        a: usize,
+        b: usize
+    }
+
+    if a == b {
+        println!("1");
+    }
+
+    if (a + b) % 2 == 0 {
+        println!("3");
+    } else {
+        println!("2");
+    }
+}
